0e355d43be8cff63ba30d0e05fc47b13fa2303ed
iamjavaexpert <techexpert7140@gmail.com>
2021-09-17 18:45:28 +0530

    Feat: Custom function( 2D model ) graph generation from user input.

--- kamodo_callbacks.py
+++ kamodo_callbacks.py
@@ -1,13 +1,18 @@
+import ast
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from kamodo import KamodoAPI
-from plotly import graph_objs as go
 
 from constants import PYSAT_URL
 
+from utils.generate_2d_graph import create_2d_graph, update_2d_graph
+
 # WORKFLOW CARDS START
+from utils.generate_3d_graph import create_3d_graph
+
 workflow_cards = html.Div(
     [
         html.Div([
@@ -227,7 +232,7 @@ def make_graph(button_id):
     return graph
 
 
-def graph_function(input_value, data_value, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, remove):
+def graph_function(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, remove):
     ctx = dash.callback_context
     new_graph = dcc.Graph(
         id='my-graph-empty',
@@ -238,9 +243,6 @@ def graph_function(input_value, data_value, n1, n2, n3, n4, n5, n6, n7, n8, n9, 
     else:
         button_id = ctx.triggered[0]["prop_id"].split(".")[0]
         print(f"PLOT GRAPH")
-
-    if input_value:
-        plot_custom_function(input_value, data_value)
 
     if button_id == "B_north-button" and n1:
         return make_graph(button_id)
@@ -291,14 +293,14 @@ def get_selected_model_names(n_clicks):
                 model_list.append(
                     dbc.ListGroup(
                         [
-                            dbc.ListGroupItem(
-                                f"{i}", id={'type': 'model-plot-button', 'index': index//2}, n_clicks=0,
-                                action=True
-                            ),
                             # dbc.ListGroupItem(
-                            #     f"{i}", className=f"model-type-button {i}-plot", id= f'{i}-button', n_clicks=0,
+                            #     f"{i}", id={'type': 'model-plot-button', 'index': index//2}, n_clicks=0,
                             #     action=True
                             # ),
+                            dbc.ListGroupItem(
+                                f"{i}", className=f"model-type-button {i}-plot", id= f'{i}-button', n_clicks=0,
+                                action=True
+                            ),
                         ]
                     )
                 )
@@ -308,17 +310,65 @@ def get_selected_model_names(n_clicks):
 
 # CUSTOM FUNCTION PLOTTING START #
 
-def plot_custom_function(input_value, data_value):
-    print(f" INPUT: {input_value} DATA: {data_value}")
+def plot_custom_function(function_value, min_value, max_value):
+
+    if not min_value:
+        min_value = -10
+    if not max_value:
+        max_value = 10
+
+    if function_value and min_value and max_value:
+        function = function_value.split('=')[1]
+        if (function.__contains__('x') or function.__contains__('X')) and (function.__contains__('y') or function.__contains__('Y')):
+            figure = create_3d_graph(function)
+        else:
+            figure = create_2d_graph(function)
+
+        if figure:
+            new_graph = dcc.Graph(
+                id='my-graph-custom',
+                figure = figure,
+            )
+            range_slider =  dcc.RangeSlider(
+                    id='my-range-slider',
+                    min=int(min_value),
+                    max=int(max_value),
+                    step=0.5,
+                    value=[-5, 5],
+                    # marks={
+                    #     0: int(min_value),
+                    #     100: int(max_value)
+                    # }
+                )
+            new_graph_area = html.Div([
+                new_graph,
+                html.Div([
+                    dbc.Row(
+                        [
+                            # dbc.Col([
+                            #     html.H5('X : ')
+                            # ], width=1),
+                            dbc.Col([
+                                range_slider
+                            ], width=12)
+                        ]
+                    ),
+                ], id='my-range-slider-area', className='my-range-slider-area')
+            ], id='my-graph-custom-area', className='my-graph-custom-area')
+
+            return new_graph_area
+        else:
+            return dbc.Alert("Input valid function only...", color="danger")
+    return False
 
 # CUSTOM FUNCTION PLOTTING END #
 
+# UPDATE CUSTOM FUNCTION GRAPH START #
 
+def update_custom_function_graph(range_value, function_value):
+    print(f"RANGE : {range_value}")
+    function = function_value.split('=')[1]
+    new_figure = update_2d_graph(range_value, function)
+    return new_figure
 
-# TESTING GRAPH FUNCTION START #
-
-def graph_function_testing(n_clicks, id):
-    print("HELLO")
-    print(f"INPUT : {n_clicks} id: {id['index']}")
-
-# TESTING GRAPH FUNCTION END #
+# UPDATE CUSTOM FUNCTION GRAPH END #
